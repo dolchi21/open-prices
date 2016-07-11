94aa62acc54dcceaba0e297e5683208f0b1da5ff
dolchi21 <axel.dolce@gmail.com>
2016-07-10 23:05:17 -0300

    drop create database

--- reset.sh
+++ reset.sh
@@ -1,3 +1,6 @@
+mysql -uroot -pdugtgz <<< "DROP DATABASE IF EXISTS open_prices"
+mysql -uroot -pdugtgz <<< "CREATE DATABASE open_prices"
+
 node sequelize.js
 node populate.js
 node populate-prices.js
