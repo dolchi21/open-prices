0b1f8cf793978a70ccae6e63bcce6b077ea9b12d
Axel Dolce <axel.dolce@gmail.com>
2017-02-23 22:22:17 -0300

    server

---
+++ src/app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static('../public'));
+
+
+
+var seneca = require('./seneca-client')
+
+
+
+app.use('/api', require('./routes/api'))
+app.use(require('./routes/services'))
+
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+app.use(function (err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.status(err.status || 500);
+    res.json(err);
+});
+
+(function printRoutes(app) {
+    //require('express-print-routes')(app, './routes.txt');
+})(app);
+
+module.exports = app;
