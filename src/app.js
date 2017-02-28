502dbc0b1a0d00a2de3885fe8e9227e4675aead7
Axel Dolce <axel.dolce@gmail.com>
2017-02-28 17:42:33 -0300

    api headers

--- src/app.js
+++ src/app.js
@@ -15,6 +15,12 @@ app.use(cookieParser());
 app.use(express.static('../public'));
 app.use('/node_modules', express.static('../node_modules'));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    next()
+})
+
 
 
 var seneca = require('./seneca-client')
@@ -26,7 +32,7 @@ app.use(require('./routes/services'))
 
 app.use('/user', require('./lib/jwt').middleware(), (req, res, next) => {
     res.json({
-        data : req.user
+        data: req.user
     })
 })
 
