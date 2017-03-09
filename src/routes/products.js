0c23e537e24ab998a1bf002ba5e7e1e7d668ff22
Axel Dolce <axel.dolce@gmail.com>
2017-03-08 23:54:02 -0300

    auth applied

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,24 +1,33 @@
 var express = require('express')
 var router = express.Router()
 
+var tokenMiddleware = require('../lib/jwt').middleware
 var ctrl = require('../controllers/products')
 
 router.get('/', ctrl.getProducts)
-router.post('/', ctrl.createProduct)
+router.post('/', tokenMiddleware(), ctrl.createProduct)
 
 router.get('/:barcode', ctrl.getProductByBarcode)
 router.get('/:barcode/price', ctrl.getProductPrice)
 router.get('/:barcode/prices', ctrl.getProductPrices)
 
-router.post('/:barcode/prices', (req, res, next) => {
+router.post('/:barcode/prices', require('../lib/jwt').middleware(), (req, res, next) => {
     var { price, date, vendor } = req.body
     var err = new Error()
     err.name = 'InvalidForm'
     if (!price || !vendor || !date) {
         return next(err)
     }
+    
+    err.message = 'No user'
+    if (!req.user) { return next(err) }
+    if (!req.user.data) { return next(err) }
+    if (!req.user.data.id) { return next(err) }
+
     next()
 })
 router.post('/:barcode/prices', ctrl.createProductPrice)
+
+router.get('/:barcode/names', ctrl.getProductNames)
 
 module.exports = router
